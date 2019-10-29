c420d90d5d295aaf04fd448c617164ce25f6d1db
Your Name <haysakj@gmail.com>
2019-10-29 08:46:11 +0200

    first commit

---
+++ Ivan.py
@@ -0,0 +1,118 @@
+# cheese=139.678945464
+# milk=18.073498
+# print("cheese={0:.2f}".format(cheese))
+# print("milk={0:.2f}".format(milk))
+
+
+# price= float(input("price="))
+# quantity= int(input("quantity="))
+# m=price*quantity
+# print("Mult={0:.3f}".format(m))
+
+
+# category=input("Category: ")
+# if category=="A":
+#     print ("Moto")
+# elif category=="B":
+#     print("Auto")
+# elif category == "C":
+#     print("Lory")
+# else:
+#     print("Eror")
+
+# num1=1`
+# while num1<=`9:
+#     num2=0
+#     while num2 <= 9:
+#         print("{0}-{1}".format(num1,num2))
+#         num2+=1
+#     num2+=1
+
+# a=float(input("a="))
+# b=float(input("b="))
+# s=a*b
+# p=2*(a+b)
+# print ("s={0}, p={1}".format(s,p))
+
+
+
+# p=float(input("price="))
+# r1=p*1*12
+# r2=p*2*12
+# r3=p*10*12
+# print("1year={0}, 2years={1}, 10years={2}".format(r1, r2, r3))
+
+
+
+
+# h=float(input("time"))
+# s=int(input("page"))
+# total_time=s*h/60
+# print("total_time={0}".format(total_time)
+
+
+
+# d=int(input("day"))
+# if 1<=d<=5:
+#     print ("workday")
+# elif 5<d<=7:
+#     print("weekend")
+# else:
+#     print("error")
+
+
+
+# print("1.Веселий")
+# print("2.Сумний")
+# print("3.Обурений")
+# answer=int(input("Ваш Вибір:"))
+# if answer==1
+#     print(":-)")
+
+# n=int(input("n="))
+# a=[int(input("el {0}:".format(i))) for i in range (n)]
+
+# def get_total (p,q,vat):
+#     return p*q*(1+vat/100)
+#
+# price=float(input("price= "))
+# quantity=float(input("quantity= "))
+# vat=float(input("vat= "))
+#
+# check= get_total(price, quantity, vat)
+# print("check={0}".format(check))
+
+#Захист лабораторних
+
+#Зад3
+
+# y=int(input("year of birth "))
+# a=2019-y
+# print("age={0}".format(a))
+
+#Зад6
+
+L=float(input("length (cm) "))
+E=int(input("effectivity (mm/h) "))
+N=int((L*10)/E)
+
+print("number of hits = {0} ".format(N))
+
+p=input("Password: ")
+
+if p=="ao3340ab":
+    print("OK")
+else:
+    print("Error")
+
+
+
+
+
+
+
+
+
+
+
+
